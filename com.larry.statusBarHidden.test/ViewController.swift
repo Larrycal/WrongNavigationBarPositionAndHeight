3e933ab3c03061f00a32989c4e1b07c3469e54aa
柳钰柯 <liuyuke@bytedance.com>
2019-09-26 17:48:17 +0800

    Initial Commit

---
+++ com.larry.statusBarHidden.test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  com.larry.statusBarHidden.test
+//
+//  Created by 柳钰柯 on 9/26/19.
+//  Copyright © 2019 柳钰柯. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
